8edd6f7feb770eabba16d2ea37f690842720c043
Jose Manuel <metalfranckie@gmail.com>
2011-07-09 02:39:54 +0000

    Agregado el proyecto, se corrigieron bugs 296,297,299 y se mejoro el codigo

---
+++ trunk/Caffeina IDE/src/DAO/LogData.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package DAO;
+
+//import javax.swing.JEditorPane;
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author JManuel
+ */
+
+public class LogData {
+    public static JTextArea epLog;
+
+    public LogData(){
+        epLog = new JTextArea();
+    }
+}
