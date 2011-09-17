a11310e2995f3ef881d440225a65e7a6d2d5fad9
Andres Zendejas <andres@caffeina.mx>
2011-09-17 19:36:54 +0000

    conexion a la base de datos

---
+++ branch/webbased_01/www/api_creation/negocios.php
@@ -0,0 +1,52 @@
+<?php
+    $BaseDatos="mini_programa";
+    $Servidor="localhost";
+    $Usuario="root";
+    $Clave="";
+
+       $Conexion_ID = mysql_connect($Servidor, $Usuario, $Clave);
+	   
+       if (!$Conexion_ID){
+          $Error = "ERROR : El intento de conexión ha fallado.";
+		  echo $Error;
+       }
+
+       if (!@mysql_select_db($BaseDatos, $Conexion_ID)){
+          $Error = "ERROR : Imposible abrir la base de datos ".$this->BaseDatos ;
+		  echo $Error;
+       }
+		$sql="Insert into metodo(nombre,tipo,sesion_valida,grupo,ejemplo_peticion,ejemplo_respuesta,descripcion) values('".$_POST["nombre_metodo"]."','".$_POST["tipo_metodo"]."',".$_POST["sesion_valida"].",".$_POST["grupo"].",'".$_POST["ejemplo_peticion"]."','".$_POST["ejemplo_respuesta"]."','".$_POST["descripcion_metodo"]."')";
+       $Consulta_ID = mysql_query($sql, $Conexion_ID);
+
+       if (!$Consulta_ID){
+		  echo mysql_error();
+       }
+		else
+		{
+			$sql="Select LAST_INSERT_ID()";
+			$Consulta_ID = mysql_query($sql, $Conexion_ID);
+			if (!$Consulta_ID){
+			echo mysql_error();
+			}
+			else
+			{
+				$id_metodo= mysql_fetch_row($Consulta_ID);
+				$sql="Insert into argumento(id_metodo,nombre,descripcion,ahuevo,tipo,default) values(".$id_metodo[0].",'".$_POST["nombre_argumento"]."','".$_POST["descripcion_argumento"]."',".$_POST["ahuevo"].",'".$_POST["tipo_argumento"]."','".$_POST["default"]."')";
+				$Consulta_ID = mysql_query($sql, $Conexion_ID);
+				if (!$Consulta_ID){
+				echo mysql_error();
+				}
+				else
+				{
+					$sql="Insert into respuesta(id_metodo,nombre,descripcion,tipo) values(".$id_metodo[0].",'".$_POST["nombre_respuesta"]."','".$_POST["descripcion_respuesta"]."','".$_POST["tipo_respuesta"]."')";
+					$Consulta_ID = mysql_query($sql, $Conexion_ID);
+					if (!$Consulta_ID){
+					echo mysql_error();
+					}
+					else
+					{
+					}
+				}
+			}
+		}
+?>
