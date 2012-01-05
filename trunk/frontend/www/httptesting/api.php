6aba1db25eb0a20c972b9a4f655ec3fb2c473035
Alan Gonzalez <alan@caffeina.mx>
2012-01-05 06:41:57 +0000

    http testing now works again

--- trunk/frontend/www/httptesting/api.php
+++ trunk/frontend/www/httptesting/api.php
@@ -2,34 +2,89 @@
 
 	require_once( "../../server/bootstrap.php" );
 
-	if(!isset($_POST["metodo"]))
-	{
-		throw new Exception("No se recibio que metodo se usara");
+	if(!isset($_POST["metodo"])){
+		die(json_encode(array("status" => "error" , "resason" => "must send metodo to call")));
 	}
   
+	function doQuery($sql){
+		if(!mysql_query($sql) ){
+			die(json_encode(array("status" => "error", "reason" => mysql_error() )));
+		}
+		
+		die(json_encode(array("status" => "ok")));
+	}
+
+
+
 	switch($_POST["metodo"])
 	{
 		case "editarPaquete" :
 			$sql = "Update httptesting_paquete_de_pruebas set pruebas = '".$_POST["pruebas"]."', nombre='".$_POST["nombre"]."', descripcion='".$_POST["descripcion"]."', locked=".$_POST["locked"]." where id_paquete_de_pruebas=".$_POST["id_paquete_de_pruebas"];
-	        break;
+			doQuery($sql);
+	    break;
 			
 		case "nuevoPaquete" :
 			$sql = "Insert into httptesting_paquete_de_pruebas(pruebas,nombre,descripcion,locked) values('".$_POST["pruebas"]."','".$_POST["nombre"]."','".$_POST["descripcion"]."',".$_POST["locked"].")";
-			break;
+			doQuery($sql);
+		break;
 			
 		case "nuevaRuta" :
 			$sql = "Insert into httptesting_ruta(nombre,ruta) values('".$_POST["nombre"]."','".$_POST["ruta"]."')";
-			break;
+			doQuery($sql);			
+		break;
 			
 		case "editarRuta" :
 			$sql = "Update httptesting_ruta set nombre = '".$_POST["nombre"]."', ruta = '".$_POST["ruta"]."' where id_ruta = ".$_POST["id_ruta"];
-			break;
-	}
-	$res = mysql_query($sql);
-	if(!$res)
-	{
-		throw new Exception(mysql_error(),mysql_errno());
-	}
+			doQuery($sql);			
+		break;
+		
+		case "test":
+
+			#########################################################
+			## parse tests
+			#########################################################
+			$Paquete = mysql_fetch_assoc(mysql_query("SELECT * FROM httptesting_paquete_de_pruebas WHERE id_paquete_de_pruebas = " . $_POST["test_id"] . ";"));
+			$Url = mysql_fetch_assoc(mysql_query("SELECT * FROM httptesting_ruta where id_ruta = " . $_POST["url"] . ";"));
+			
+			$tparser = new TestParser( $Paquete["pruebas"] );
+
+			try{
+				$tparser->parse();
+
+			}catch(Exception $e){
+				die(json_encode(array("status" => "error", "reason" => $e )));
+
+			}
+
+			#########################################################
+			## bit of configuration
+			#########################################################	
+			HTTPClient::setUrlBase( $Url["ruta"] );
 
 
-?>
+
+			#########################################################
+			## start testing
+			#########################################################
+			$output = array();
+			while($tparser->hasNextTest())
+			{
+
+				$tester = new Tester( $tparser->nextTest() );
+
+				try{
+					$tester->test();
+
+				}catch(Exception $e){
+					die(json_encode(array("status" => "error", "reason" => $e )));
+				}
+
+			}
+
+
+		berak;
+		
+		
+	}
+	
+
--- trunk/frontend/www/httptesting/index.php
+++ trunk/frontend/www/httptesting/index.php
@@ -3,156 +3,210 @@
 	require_once("../../server/bootstrap.php");
 
 ?><!DOCTYPE html>
-<html xmlns="http://www.w3.org/1999/xhtml" lang="en" >
+<html xmlns="http://www.w3.org/1999/xhtml" lang="en">
 <head>
-
-<title>HTTP Testing | Caffeina WebFramework</title>
-
-
-<link type="text/css" rel="stylesheet" href="../media/f.css"/>
-
-</head>
-<body class="safari4 mac Locale_en_US">
-<input type="hidden" autocomplete="off" id="post_form_id" name="post_form_id" value="d8f38124ed9e31ef3947198c6d26bff1"/>
-<div id="FB_HiddenContainer" style="position:absolute; top:-10000px; width:0px; height:0px;">
-</div>
-<div class="devsitePage">
-	<div class="menu">
-		<div class="content">
-			<a class="logo" href="../index.php">
-				
-				<!-- https://s-static.ak.facebook.com/rsrc.php/v1/yW/r/N2f0JA5UPFU.png -->
-				<img class="img" src="../media/cwhite.png" alt="Facebook Developers" width="166" height="17"/>
-				
-
-				
-			</a>
-
-
-			<a class="l" href="index.php">UnitTester</a>
-			<a class="l" href="../apigen/">ApiGen</a>
+	<script type="text/javascript" charset="utf-8" src="https://ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.min.js"></script>
+	<title>HTTP Testing | Caffeina WebFramework</title>
+	<link type="text/css" rel="stylesheet" href="../media/f.css"/>
+	<script type="text/javascript" charset="utf-8">
+	
+		$.extend({
+		  getUrlVars: function(){
+		    var vars = [], hash;
+		    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+		    for(var i = 0; i < hashes.length; i++)
+		    {
+		      hash = hashes[i].split('=');
+		      vars.push(hash[0]);
+		      vars[hash[0]] = hash[1];
+		    }
+		    return vars;
+		  },
+		  getUrlVar: function(name){
+		    return $.getUrlVars()[name];
+		  }
+		});
+		
+		
+		
+		var httptesting = {
 			
+			ajax : function(p, c){
+				
+				$.ajax({
+					url: "api.php",
+					type : "POST",
+					data: p,
+					success: function(a,b,parseOnReturn){
+						var r;
+						if(parseOnReturn !== true){
+							if(c === undefined){
+								alert("ok");
+							}else{
+								c.call(null, a);							
+							}
+							return;
+						}
+						try{
+				    		r = $.parseJSON(a);
+						}catch( e){
+							console.error(e);
+							alert("Error");
+							return;
+						}
+						
+						if(r.status != "ok"){
+							alert("error");
+							return;
+						}
+						
+						if(c === undefined){
+							alert("ok");
+						}else{
+							c.call(null, r);							
+						}
 
 
-			<div class="clear">
+				  	}
+				});
+			},
+			
+			test : function(){
+				
+				//Test params
+				var url 	= $.getUrlVar('url'),
+					test 	= $.getUrlVar('test');
+				
+				if(
+					url === undefined 
+					|| test === undefined
+				){
+					alert("Faltan parametros");
+					return;
+				}
+				
+				var callback = function( res ){
+					$("#response").html(res).show();
+				}
+				
+				$("textarea").hide();
+				
+				this.ajax({
+					metodo	: "test",
+					url 	: url,
+					test_id	: test
+				},callback, false);
+			} 
+			
+		}
+	</script>
+</head>
+<body class="mac Locale_en_US">
+	<div class="devsitePage">
+		<div class="menu">
+			<div class="content">
+				<a class="logo" href="../index.php">
+				<img class="img" src="../media/cwhite.png" alt="Facebook Developers" width="166" height="17"/>
+				</a>
+				<a class="l" href="index.php">UnitTester</a>
+				<a class="l" href="../apigen/">ApiGen</a>
+				<div class="clear">
+				</div>
 			</div>
 		</div>
-	</div>
-	<div class="body nav">
-		<div class="content">
-			<div id="bodyMenu" class="bodyMenu">
-				<div>
-					<h3>Paquete</h3>
-				</div>
-				<div>
-					<h3>Paquete</h3>
-				</div>				
-				<div class="toplevelnav">
-					<ul>
-						
-					</ul>
-				</div>
-
-				<ul id="navsubsectionpages">
-					
-				</ul>
+		<div class="body nav">
+			<div class="content">
+				<div id="bodyMenu" class="bodyMenu">
+					<?php
+						$s = mysql_query("select * from httptesting_paquete_de_pruebas");
+						while( ($row = mysql_fetch_assoc($s))  != null){
+							if(isset($_GET["test"]) && $_GET["test"] == $row["id_paquete_de_pruebas"]){
+								$style = "background: gray; ";							
+							}else{
+								$style = "";
+							}
+							
+							echo "<div style='". $style ."'>"; 
+							$hop = "?test=" . $row["id_paquete_de_pruebas"] . "&" ; 
+							if(isset( $_GET["url"] )){ 
+								$hop .= "url=" . $_GET["url"]; 
+							} 
+							echo "<a href='".$hop."'><h3 style='margin-bottom:0px'>" . $row["nombre"] . "</h3></a>"; 
+							echo "<p>" . $row["descripcion"] . "</p>"; 
+							echo "</div>"; 
+						} 
+						?>
 			</div>
 			<div id="bodyText" class="bodyText">
 				<div class="header">
 					<div class="content">
-						
-						
 						<h1>HTTP REST API Test Suite</h1>
-						<!--
-						<div class="breadcrumbs">
-							<a href=".">POS ERP</a> 
-						</div>
-						-->
-						<p>Hacer llamadas HTTP al API. </p>
-
+						<p>
+							Hacer llamadas HTTP al API.
+						</p>
 					</div>
 				</div>
-
-</head>
-				<form action="test.php" method="POST" id="forma" >
-
-
-
-
-<body>
-<form>
-<span id="selector">
-	URL de pruebas
-	<select name="cSelect" >
-		<?php
-			/**
-			 * Obtener las urls
-			 * 
-			 * */
-			$res = mysql_query("select * from httptesting_ruta");
-			
-			while( ($row = mysql_fetch_assoc($res)) != null ){
-
-				if(isset($_GET["url"]) && $_GET["url"] == $row["id_ruta"]){
-					echo "<option selected id='". $row["id_ruta"] ."'>" . $row["nombre"] . " | " . $row["ruta"] . "adsfadsf</option>";
-				}else{
-					echo "<option id='". $row["id_ruta"] ."'>" . $row["nombre"] . " | " . $row["ruta"] . "adsfadsf</option>";
-				}
-			}
-		?>
-	</select>
-</span>
-</form>
-
-<br>
-
-<textarea cols="80"  rows="50" name="tests"  ><?php
-
-	$tests  = mysql_fetch_assoc(mysql_query("select * from http_tests limit 1;")) or die (mysql_error());
-	echo $tests["tests"]; 
-
-?>
-
-</textarea>
-					<br>
-						
-					</form>
-	 		
+				<span id="selector">
+				URL de pruebas
+				<select name="cSelect">
+					<?php
+					/**
+					 * Obtener las urls
+					 * 
+					 * */
+					$res = mysql_query("select * from httptesting_ruta");
+					while( ($row = mysql_fetch_assoc($res)) != null ){
+						if(isset($_GET["url"]) && $_GET["url"] == $row["id_ruta"]){
+							echo "<option selected id='". $row["id_ruta"] ."'>" . $row["nombre"] . " | " . $row["ruta"] . "</option>"; 
+						}else{ 
+							echo "<option id='". $row["id_ruta"] ."'>" . $row["nombre"] . " | " . $row["ruta"] . "</option>"; 
+						} 
+					} 
+					
+					?>
+				</select>
+				<input type="button" name="" value="Iniciar pruebas" onClick="httptesting.test()" >
 				
-				<!-- ----------------------------------------------------------------------
-					 ---------------------------------------------------------------------- -->
+				</span>
 				<hr/>
-
+				<div id="response">
+					
+				</div>
+				<?php
+				$s = mysql_query("select * from httptesting_paquete_de_pruebas");
+				while( ($row = mysql_fetch_assoc($s))  != null){
+					if(isset($_GET["test"]) && $_GET["test"] == $row["id_paquete_de_pruebas"]){
+						echo "<div style=''>"; 
+					}else{ 
+						echo "<div style='display:none'>"; 
+					} 
+					
+					echo '<textarea disabled cols="80" rows="20" name="tests">' . $row["pruebas"] . "</textarea>"; 
+					echo "</div>"; 
+				} 
+				?>
 				<div class="mtm pvm uiBoxWhite topborder">
 					<div class="mbm">
-						
 					</div>
 					<abbr title="Monday, September 5, 2011 at 8:28pm" data-date="Mon, 05 Sep 2011 18:28:49 -0700" class="timestamp">Ultima modifiacion</abbr>
 				</div>
-
 			</div>
-
 			<div class="clear">
 			</div>
-
 		</div>
 	</div>
 	<div class="footer">
 		<div class="content">
 			<div class="copyright">
-
 			</div>
 			<div class="links">
-				<a href="https://www.facebook.com/platform">About</a><a href="/policy/">Platform Policies</a><a href="https://www.facebook.com/policy.php">Privacy Policy</a>
+				<a href="">About</a>
+				<a href="">Platform Policies</a>
+				<a href="">Privacy Policy</a>
 			</div>
 		</div>
 	</div>
-
-
-
-	
-</div>
-
+	</div>
 </body>
 </html>
 
