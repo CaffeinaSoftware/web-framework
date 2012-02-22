93bf5f8baa843c957b58fbada257baccd1dd1586
Andres Zendejas <andres@caffeina.mx>
2012-02-22 20:10:13 +0000

    Olvide comentar la bandera que use para mi compu, para usar la variable $_SERVER["http_user"] xD

--- trunk/frontend/www/apigen/index.php
+++ trunk/frontend/www/apigen/index.php
@@ -2,10 +2,10 @@
 
 	require_once("../../server/bootstrap.php");
         
-        if(!isset($_SERVER["http_user"]))
-        {
-            $_SERVER["http_user"] = "andres";
-        }
+//        if(!isset($_SERVER["http_user"]))
+//        {
+//            $_SERVER["http_user"] = "andres";
+//        }
 ?>
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" >
